387d74a226888eb8f0f5737efb3fe03b4fdde0aa
Oriol Moreno <46903766+OriolMoreno@users.noreply.github.com>
2020-04-27 19:07:10 +0200

    Added perspective transform

--- modules/vision/cardDetection.py
+++ modules/vision/cardDetection.py
@@ -6,7 +6,7 @@ import imutils
 from imutils import contours
 
 CARD_MAX_AREA = 120000
-CARD_MIN_AREA = 25000
+CARD_MIN_AREA = 20000
 
 
 def detectCards(image):
@@ -26,10 +26,10 @@ def detectCards(image):
 
     _, thresh_image = cv2.threshold(blur, thresh_level, 255, cv2.THRESH_BINARY)
 
-    # plt.imshow(thresh_image)
-    # plt.show()
+    plt.imshow(thresh_image)
+    plt.show()
 
-    cnts, parent = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cnts, parent = cv2.findContours(thresh_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     index_sort = sorted(range(len(cnts)), key=lambda i: cv2.contourArea(cnts[i]), reverse=True)
 
     if len(cnts) == 0:
@@ -53,6 +53,7 @@ def detectCards(image):
     # 2), bigger area than the minimum card size, 3) have no parents,
     # and 4) have four corners
     realCards = []
+    cardCorners = []
 
     for i in range(len(cnts_sort)):
         size = cv2.contourArea(cnts_sort[i])
@@ -63,17 +64,51 @@ def detectCards(image):
                 and (hier_sort[i][3] == -1) and (len(approx) == 4)):
             cnt_is_card[i] = 1
             realCards.append(cnts_sort[i])
+            cardCorners.append(approx)
+    return realCards, cardCorners
 
-    return realCards
 
-
-def extractCards(img, cardContours):
+def extractCards(image, cardContours, cardCorners):
     cards = []
+    i = 0
     for contour in cardContours:
-        x, y, w, h = cv2.boundingRect(contour)
-        card = img[y:y + h, x:x + w]
-        plt.imshow(card)
-        plt.show()
-        # TODO: Perspective transform
+        rect = np.zeros((4, 2), dtype="float32")
 
+        rect[0] = cardCorners[i][1][0]  # top left
+        rect[1] = cardCorners[i][0][0]  # top right
+        rect[2] = cardCorners[i][3][0]  # bot right
+        rect[3] = cardCorners[i][2][0]  # bot left
+
+        # TODO: Check if cards are horizontal or vertical if vertical rotate
+        w = rect[1] - rect[0]
+        h = rect[2] - rect[3]
+
+        print(w,h)
+
+        x, y, w, h = cv2.boundingRect(contour)
+
+        (tl, tr, br, bl) = rect
+
+        maxWidth = 175
+
+        maxHeight = 210
+        # now that we have the dimensions of the new image, construct
+        # the set of destination points to obtain a "birds eye view",
+        # (i.e. top-down view) of the image, again specifying points
+        # in the top-left, top-right, bottom-right, and bottom-left
+        # order
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype="float32")
+        # compute the perspective transform matrix and then apply it
+        M = cv2.getPerspectiveTransform(rect, dst)
+        warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
+        #card = img[y:y + h, x:x + w]
+
+        plt.imshow(warped)
+        plt.show()
+
+        i += 1
     return None
