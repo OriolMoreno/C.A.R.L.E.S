454e4cb05cbe9a26b3a7dd65eded2df16777a80e
janMoros <48655384+janMoros@users.noreply.github.com>
2020-05-02 20:01:06 +0200

    match simulation almost complete
    només em falten uns errors a l'hora de calcular la puntuació

--- modules/BriscaPlayer/simulation.py
+++ modules/BriscaPlayer/simulation.py
@@ -5,7 +5,7 @@ def GenerarBaralla():
     baralla = []
     pals = ['b', 'e', 'o', 'c']
     for pal in pals:
-        for i in range(1, 13):
+        for i in range(1, 4):
             baralla.append((i, pal))
     return baralla
 
@@ -18,10 +18,11 @@ def iniciarPartida(baralla):
         p.ma.append(baralla.pop())
     return p
 
-def pintarTaulellObert(p, maH,cartesTaula): #anar ampliant
+def pintarTaulellObert(p, maH,cartesTaula,baralla): #anar ampliant
     print("Ma de la IA: ", p.ma)
     print("Pinta: ", p.pinta)
     print("Cartes sobre la Taula: ", cartesTaula)
+    print("Nombre de cartes a la pila:", len(baralla))
     print("Ma del jugador: ", maH)
     print(" ")
 
@@ -46,7 +47,7 @@ def humaJugaCarta(p,cartesTaula,maH): # simulació de que l'humà tira una carta
     if cartesTaula:
         print("En C.A.R.L.E.S ha jugat un ", nomCarta(cartesTaula[0]))
         print(" ")
-        pintarTaulellObert(p,maH,cartesTaula)
+        #pintarTaulellObert(p,maH,cartesTaula)
 
     print("A la teva ma tens les següents cartes: ",maH)
     print("Quina vols jugar? (1, 2 o 3)")
@@ -61,12 +62,12 @@ def humaJugaCarta(p,cartesTaula,maH): # simulació de que l'humà tira una carta
 def IArecullCartesGuanyades(cartesGuanyadesIA,cartesTaula,p):
     for i in range(2):
         p.puntsIA += p.valorNumeros[cartesTaula[0][0]]
-        cartesGuanyadesIA.append(cartesTaula.pop())
+        cartesGuanyadesIA.append(cartesTaula.pop(0))
 
 def HrecullCartesGuanyades(cartesGuanyadesH,cartesTaula,p):
     for i in range(2): #,carta in enumerate(cartesTaula):
         p.puntsH += p.valorNumeros[cartesTaula[0][0]]
-        cartesGuanyadesH.append(cartesTaula.pop())
+        cartesGuanyadesH.append(cartesTaula.pop(0))
 
 def simulacioPartidaObert():
     baralla = GenerarBaralla()
@@ -84,13 +85,13 @@ def simulacioPartidaObert():
     cartesGuanyadesIA = []
     cartesGuanyadesH = []
 
-    pintarTaulellObert(p,maHuma,cartesSobreLaTaula)
+    pintarTaulellObert(p,maHuma,cartesSobreLaTaula,baralla)
 
     # comença sempre tirant primer la IA, per simplificar
     ### Fer que digui quelcom gracioset ###
     comencaH = False
 
-    while len(baralla) > 0 or p.ma:
+    while len(baralla) > 0:
         if not comencaH: # Comença tirant la IA
             cartaATirar = p.TriarCarta(False)
             # El robot juga la carta escollida
@@ -99,7 +100,7 @@ def simulacioPartidaObert():
             # pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
             # L'humà juga una carta
             humaJugaCarta(p,cartesSobreLaTaula,maHuma)
-            pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+            #pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
 
             if p.guanyaIA(cartesSobreLaTaula[0],cartesSobreLaTaula[1],False): #cartaIA, cartaH, comencaH
                 print("Aquesta ronda la guanya en C.A.R.L.E.S.! \n")
@@ -110,8 +111,8 @@ def simulacioPartidaObert():
                 print("En C.A.R.L.E.S. roba una carta")
                 IARoba(baralla,p)
                 print("Robes una carta\n")
-                humaRoba(baralla,maHuma)
-                pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+                if len(baralla) > 0: humaRoba(baralla,maHuma)
+                pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
             else:
                 print("Guanyes aquesta ronda!\n")
                 # L'humà recull de la taula les cartes guanyades
@@ -120,19 +121,19 @@ def simulacioPartidaObert():
                 print("Robes una carta")
                 humaRoba(baralla, maHuma)
                 print("En C.A.R.L.E.S. roba una carta\n")
-                IARoba(baralla, p)
-                pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+                if len(baralla) > 0: IARoba(baralla, p)
+                pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
                 comencaH = True
         else: # comença l'humà
             # L'humà juga una carta
             humaJugaCarta(p, cartesSobreLaTaula, maHuma)
-            pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+            pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
 
             cartaATirar = p.TriarCarta(True,cartesSobreLaTaula[0])
             # El robot juga la carta escollida
             robotJugaCarta(p, cartaATirar, cartesSobreLaTaula)
             print("En C.A.R.L.E.S. juga un ", nomCarta(cartesSobreLaTaula[1]))
-            pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+            #pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
 
             if p.guanyaIA(cartesSobreLaTaula[1],cartesSobreLaTaula[0],True): #cartaIA, cartaH, comencaH
                 print("Aquesta ronda la guanya en C.A.R.L.E.S.! \n")
@@ -143,8 +144,8 @@ def simulacioPartidaObert():
                 print("En C.A.R.L.E.S. roba una carta")
                 IARoba(baralla,p)
                 print("Robes una carta\n")
-                humaRoba(baralla,maHuma)
-                pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+                if len(baralla) > 0: humaRoba(baralla,maHuma)
+                pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
                 comencaH = False
             else:
                 print("Guanyes aquesta ronda!\n")
@@ -154,10 +155,77 @@ def simulacioPartidaObert():
                 print("Robes una carta")
                 humaRoba(baralla, maHuma)
                 print("En C.A.R.L.E.S. roba una carta\n")
-                IARoba(baralla, p)
-                pintarTaulellObert(p, maHuma, cartesSobreLaTaula)
+                if len(baralla) > 0: IARoba(baralla, p)
+                pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
+
+    if len(maHuma) == 2:
+        # L'humà robarà la pinta si és la única carta que queda
+        maHuma.append(p.pinta[:])
+    elif len(p.ma) == 2:
+        # El robot faria el mateix
+        ### Funcio perquè el robot ho faci
+        p.ma.append(p.pinta[:])
+    else:
+        print("No m'agraden les trampes")
+        return
 
 
+
+
+
+    # Últimes 3 cartes. A la millora, minmax
+    for i in range(3):
+        if not comencaH: # Comença tirant la IA
+            cartaATirar = p.TriarCarta(False)
+            # El robot juga la carta escollida
+            robotJugaCarta(p,cartaATirar,cartesSobreLaTaula)
+            # L'humà juga una carta
+            humaJugaCarta(p,cartesSobreLaTaula,maHuma)
+            pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
+
+            if p.guanyaIA(cartesSobreLaTaula[0],cartesSobreLaTaula[1],False): #cartaIA, cartaH, comencaH
+                print("Aquesta ronda la guanya en C.A.R.L.E.S.! \n")
+                # El robot mou les cartes guanyades a l'slot corresponent
+                IArecullCartesGuanyades(cartesGuanyadesIA,cartesSobreLaTaula,p)
+            else:
+                print("Guanyes aquesta ronda!\n")
+                # L'humà recull de la taula les cartes guanyades
+                HrecullCartesGuanyades(cartesGuanyadesH, cartesSobreLaTaula, p)
+                comencaH = True
+        else: # comença l'humà
+            # L'humà juga una carta
+            humaJugaCarta(p, cartesSobreLaTaula, maHuma)
+            pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
+
+            cartaATirar = p.TriarCarta(True,cartesSobreLaTaula[0])
+            # El robot juga la carta escollida
+            robotJugaCarta(p, cartaATirar, cartesSobreLaTaula)
+            print("En C.A.R.L.E.S. juga un ", nomCarta(cartesSobreLaTaula[1]))
+            pintarTaulellObert(p, maHuma, cartesSobreLaTaula,baralla)
+
+            if p.guanyaIA(cartesSobreLaTaula[1],cartesSobreLaTaula[0],True): #cartaIA, cartaH, comencaH
+                print("Aquesta ronda la guanya en C.A.R.L.E.S.! \n")
+                # El robot mou les cartes guanyades a l'slot corresponent
+                IArecullCartesGuanyades(cartesGuanyadesIA,cartesSobreLaTaula,p)
+                comencaH = False
+            else:
+                print("Guanyes aquesta ronda!\n")
+                # L'humà recull de la taula les cartes guanyades
+                HrecullCartesGuanyades(cartesGuanyadesH, cartesSobreLaTaula, p)
+
+
+    # Final de la partida:
+    print("No cal que comptis les cartes, ja ho he fet jo")
+    print("Tu has aconseguit",p.puntsH,"punts,")
+    print("I en C.A.R.L.E.S. ,", p.puntsIA, ".")
+    print("Per tant...")
+    if p.puntsH > p.puntsIA:
+        print("Has guanyat, enhorabona!")
+    else:
+        print("Ha guanyat en C.A.R.L.E.S.")
+        print("Sempre pots tornar-ho a intentar: Ell no es cansarà mai, li encanta la brisca!")
+    print(cartesGuanyadesIA)
+    print(cartesGuanyadesH)
 
 if __name__ == '__main__':
     simulacioPartidaObert()
